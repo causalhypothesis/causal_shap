f44ce17e6f3f637d94bf859eccea33cda12041d6
maksDev123 <maksym.buleshnyi.pn@ucu.edu.ua>
2024-10-22 16:59:09 +0300

    fix: fix linting

--- shapley_values/causal_shap.py
+++ shapley_values/causal_shap.py
@@ -11,6 +11,7 @@ from typing import Any
 
 random.seed(42)
 
+
 class ShapleyValuesType(Enum):
     STANDART = 'STANDART'
     MARGINAL = 'MARGINAL'
@@ -28,16 +29,17 @@ class Explainer(BaseModel):
         super().__init__(**data)
         self.X = np.round(self.X, 1)
         self.X_counter = collections.Counter(map(tuple, self.X))
-    
-    def compute_shapley_values(self, sample, type = ShapleyValuesType.MARGINAL, causal_struct = None):
+
+    def compute_shapley_values(self, sample, type=ShapleyValuesType.MARGINAL, causal_struct=None):
         if type == ShapleyValuesType.CAUSAL and not causal_struct:
-            raise Exception("Causal graph has to be provided for computing Causal Shapley Values")
+            raise Exception(
+                "Causal graph has to be provided for computing Causal Shapley Values")
         n_features = self.X.shape[-1]
         sample = np.round(sample, 1)
         phis = {}
         for feature in range(n_features):
             local_shap_score = self.approximate_shapley(feature, sample, type,
-                                                 causal_struct)
+                                                        causal_struct)
             phis[str(feature + 1)] = local_shap_score[0]
 
         # Check if the sum of the Shapley values and expected value adds up to the prediction
@@ -45,11 +47,12 @@ class Explainer(BaseModel):
         f_x = get_baseline(self.X, self.model)
         print("Baseline f(x): ", f_x)
         print("Predicted f(x): ", self.model.predict(x))
-        print("Sigma_phi + E(fX): ", round(float(sum(list(phis.values())) + f_x), 3))
+        print("Sigma_phi + E(fX): ",
+              round(float(sum(list(phis.values())) + f_x), 3))
 
         return phis
 
-    def approximate_shapley(self, xi, x, type, causal_struct = None):
+    def approximate_shapley(self, xi, x, type, causal_struct=None):
         N = self.X.shape[-1]
         m = mt.factorial(N)
         R = list(itertools.permutations(range(N)))
@@ -59,7 +62,7 @@ class Explainer(BaseModel):
         vf1, vf2 = 0, 0
         for i in range(m):
             abs_diff, f1, f2 = self.get_value(type, list(R[i]), x, causal_struct,
-                                        xi)
+                                              xi)
 
             vf1 += f1
             vf2 += f2
@@ -71,7 +74,7 @@ class Explainer(BaseModel):
         if count_negative < 0:
             score = -1 * score
         return score / m
-    
+
     def get_value(self, type, permutation, x, causal_struct, xi):
         N = self.X.shape[-1]
 
@@ -86,7 +89,7 @@ class Explainer(BaseModel):
         for j in indices:
             x_hat[j] = x[j]
             x_hat_2[j] = x[j]
-        
+
         baseline_check_1, baseline_check_2 = [], []
         f1, f2 = 0, 0
         indices_baseline_2 = indices_baseline[:]
@@ -102,21 +105,22 @@ class Explainer(BaseModel):
                 baseline_check_1.append(x_hat.tolist())
                 match type:
                     case ShapleyValuesType.MARGINAL:
-                        prob_x_hat = get_probability(self.X_counter, x_hat, indices_baseline, lenX)
+                        prob_x_hat = get_probability(
+                            self.X_counter, x_hat, indices_baseline, lenX)
                     case ShapleyValuesType.CONDITIONAL:
-                        prob_x_hat = conditional_prob(self.X_counter, x_hat, indices, indices_baseline, lenX)
+                        prob_x_hat = conditional_prob(
+                            self.X_counter, x_hat, indices, indices_baseline, lenX)
                     case ShapleyValuesType.CAUSAL:
-                        prob_x_hat = causal_prob(self.X_counter, x_hat, indices, indices_baseline, causal_struct, lenX)
+                        prob_x_hat = 0.1  # Implementation for causal
 
-                x_hat = np.reshape(x_hat, (1, N))            
+                x_hat = np.reshape(x_hat, (1, N))
                 f1 = f1 + (self.model.predict_proba(x_hat)[0][1] * prob_x_hat if self.is_classification else self.model.predict(
                     x_hat) * prob_x_hat)
-            
 
             # xi index will be given to baseline for f2
             x_hat_2[xi] = X[xi]
             if xi not in indices_baseline_2:
-               indices_baseline_2.append(xi)
+                indices_baseline_2.append(xi)
 
             # No repetition
             indices_2 = indices[:]
@@ -125,12 +129,14 @@ class Explainer(BaseModel):
                 baseline_check_2.append(x_hat_2.tolist())
                 match type:
                     case ShapleyValuesType.MARGINAL:
-                        prob_x_hat_2 = get_probability(self.X_counter, x_hat_2, indices_baseline_2, lenX)
+                        prob_x_hat_2 = get_probability(
+                            self.X_counter, x_hat_2, indices_baseline_2, lenX)
                     case ShapleyValuesType.CONDITIONAL:
-                        prob_x_hat_2 = conditional_prob(self.X_counter, x_hat_2, indices_2, indices_baseline_2, lenX)
+                        prob_x_hat_2 = conditional_prob(
+                            self.X_counter, x_hat_2, indices_2, indices_baseline_2, lenX)
                     case ShapleyValuesType.CAUSAL:
                         prob_x_hat_2 = causal_prob(self.X_counter, x_hat_2, indices_2, indices_baseline_2, causal_struct,
-                                                lenX)
+                                                   lenX)
 
                 x_hat_2 = np.reshape(x_hat_2, (1, N))
                 f2 = f2 + (self.model.predict_proba(x_hat_2)[0][1] * prob_x_hat_2 if self.is_classification else self.model.predict(
--- shapley_values/probabilities.py
+++ shapley_values/probabilities.py
@@ -15,30 +15,8 @@ def get_probability(unique_count, x_hat, indices_baseline, n):
 
 def conditional_prob(unique_count, x_hat, indices, indices_baseline, n):
     numerator_indices = indices + indices_baseline
-    print(f"numerator_indices: {numerator_indices}")
     numerator = get_probability(unique_count, x_hat, numerator_indices, n)
-
-    print(f"Indices {indices}")
-    print(x_hat)
     denominator = get_probability(unique_count, x_hat, indices, n)
-    print(f"numerator: {numerator}")
-    print(f"Denominator: {denominator}")
-    print("----------------")
-
     return  numerator / (denominator + 1e-7)
-
-def causal_prob(unique_count, x_hat, indices, indices_baseline, causal_struc, n):
-    p = 1
-    for i in indices_baseline:
-        intersect_s, intersect_s_hat = [], []
-        intersect_s_hat.append(i)
-        if len(causal_struc[str(i)]) > 0:
-            for index in causal_struc[str(i)]:
-                if index in indices or index in indices_baseline:
-                    intersect_s.append(index)
-            p *= conditional_prob(unique_count, x_hat, intersect_s, intersect_s_hat, n)
-        else:
-            p *= get_probability(unique_count, x_hat, intersect_s_hat, n)
-    return p
 
 
